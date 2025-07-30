7a8d63bb8f2878f1dada0716965f66aadda7d48c
pradeep-singh911 <psksuncity@gmail.com>
2025-07-30 13:24:23 +0530

    This is for clone

--- first.c
+++ first.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-    int i=0;
-    while(i<50){
+    int i=10;
+    while(i<50)
+    {
         printf("%d\n",i);
         
-        i=i+1;
+        i = i + 1;
     }
     return 0;
 }
